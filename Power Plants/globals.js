1c8ed1f371cb3a7ff498930418f98d995277a331
LelouchVDK <christian.rettrup@gmail.com>
2024-12-09 12:00:28 +0100

    Added the global variables

---
+++ Power Plants/globals.js
@@ -0,0 +1,4 @@
+let currentPoints = document.getElementById('currentPoints');
+let currentPlant = document.getElementById('currentPlant');
+let currentSubject = document.getElementById('currentSubject');
+let waterCount = 0;
